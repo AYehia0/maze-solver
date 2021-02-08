9904b0820480bf665818f79658f4108e83d1c870
AYehia0 <ahmedyehia611@gmail.com>
2021-02-08 03:48:06 +0200

    updated main

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ from maze import Maze
 m = Maze('maze_map.txt')
 m.read_map()
 m.draw_map()
+print(f"Start Position:{m.start}, Goal Position:{m.goal}")
+print(len(m.solve_map(m.map, m.start, m.goal)))
