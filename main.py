5b1e0037a8ecb7fc68a4390a7032081e26384d0a
AYehia0 <ahmedyehia611@gmail.com>
2021-02-08 04:41:38 +0200

    showing the status after the loop ends

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ from maze import Maze
 
 m = Maze('maze_map.txt')
 m.read_map()
-print(f"Start Position:{m.start}, Goal Position:{m.goal}")
 path = m.solve_map(m.map, m.start, m.goal)
 m.draw_steps(path)
