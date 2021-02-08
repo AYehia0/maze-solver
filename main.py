93469376687fca5714c2274028e352382ed69095
AYehia0 <ahmedyehia611@gmail.com>
2021-02-08 04:16:17 +0200

    nice ASCII animations lol

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from maze import Maze
 
 m = Maze('maze_map.txt')
 m.read_map()
-m.draw_map()
 print(f"Start Position:{m.start}, Goal Position:{m.goal}")
-print(len(m.solve_map(m.map, m.start, m.goal)))
+path = m.solve_map(m.map, m.start, m.goal)
+m.draw_steps(path)
